07b7a0a6b00429b5941da3b319cf07a3f390cf68
Jon <jon@MacBook-Pro.local>
2014-01-24 16:24:16 -0600

    Initial Commit

---
+++ Cricket Scoreboard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Cricket Scoreboard
+//
+//  Created by Jon on 1/24/14.
+//  Copyright (c) 2014 Jon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Cricket Scoreboard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cricket Scoreboard
+//
+//  Created by Jon on 1/24/14.
+//  Copyright (c) 2014 Jon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
